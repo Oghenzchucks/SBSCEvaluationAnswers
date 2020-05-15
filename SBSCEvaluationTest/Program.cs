eaa6ff5bfa369024a93084a1cbf141c8d59b25cc
oghenzchucks <oghenzchucks@gmail.com>
2020-05-15 20:35:19 +0100

    New Project

---
+++ SBSCEvaluationTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SBSCEvaluationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
