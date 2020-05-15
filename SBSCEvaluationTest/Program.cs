8ef1f38900e053be668a3d75f9e4be561784655e
oghenzchucks <oghenzchucks@gmail.com>
2020-05-15 21:48:05 +0100

    Update to questions 1, 2 and 3

--- SBSCEvaluationTest/Program.cs
+++ SBSCEvaluationTest/Program.cs
@@ -10,7 +10,11 @@ namespace SBSCEval
             #region Question 1
 
             // Input string.
-            string string1 = "1-2-2-4-5";
+
+            // Uncomment the strings below to check them both
+
+            //string string1 = "1-2-2-4-5";
+            string string1 = "3-4-5-6-7";
 
             // Split string on hyphens (this will separate all the numbers).
             string[] words = string1.Split('-');
@@ -21,9 +25,9 @@ namespace SBSCEval
                 Console.WriteLine("WORD: " + word);
             }
 
-
             // Calculates the difference between first and the next
-            int k;
+
+            int[] diff = new int[8];
 
             for (int i = 0; i < words.Length; i++)
             {
@@ -31,16 +35,37 @@ namespace SBSCEval
 
                 if (i > 0 - 1 && i < words.Length - 1)
                 {
-                    k = int.Parse(words[i + 1]) - int.Parse(words[i]);
-                    Console.WriteLine("Difference: " + k);
-                }
+                    diff[i] = int.Parse(words[i + 1]) - int.Parse(words[i]);
 
+                    Console.WriteLine("Difference: " + diff[i]);
+                }
             }
+
+            // Checks to see if all values in the array are the same.
+            bool result = Array.TrueForAll(diff, y => diff[0] == diff[1]);
+
+
+            // Output the final result
+            Console.WriteLine("TRUEFORALL: {0}", result);
 
             #endregion
 
-            #region Question 2
 
+
+            #region Question 2
+            int[] arr1 = new int[] { 1, 4, 6, 9 };
+            for(int i = 0; i < arr1.Length; i++)
+            {
+                if(i < arr1.Length - 1)
+                {
+                    int d = arr1[i + 1] - arr1[i];
+
+                    int[] arr2 = new int[d];
+
+                    Console.WriteLine(d);
+                }
+                
+            }
 
             #endregion
 
@@ -48,6 +73,8 @@ namespace SBSCEval
             #region Question 3
             // Input string.
             string string3 = "occurrence";
+
+            // separate all characters in string with each one occuring once
             char ch1 = 'o';
             char ch2 = 'c';
             char ch3 = 'u';
@@ -55,6 +82,7 @@ namespace SBSCEval
             char ch5 = 'e';
             char ch6 = 'n';
 
+            // Counts how many times the character occurs
             int freq1 = string3.Count(f => (f == ch1));
             int freq2 = string3.Count(f => (f == ch2));
             int freq3 = string3.Count(f => (f == ch3));
@@ -62,6 +90,7 @@ namespace SBSCEval
             int freq5 = string3.Count(f => (f == ch5));
             int freq6 = string3.Count(f => (f == ch6));
 
+            // Prints the final results
             Console.WriteLine(ch1.ToString() + freq1 + ch2.ToString() + freq2 + ch3.ToString() + freq3 + ch4.ToString() + freq4 + ch5.ToString() + freq5 + ch6.ToString() + freq6);
 
 
